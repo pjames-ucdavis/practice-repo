ca3ea02313490babd1e83132006688c5ec635392
Arijit Nandi <arijit.k.nandi@gmail.com>
2025-06-10 16:53:20 -0400

    conflict creator

--- app.R
+++ app.R
@@ -38,7 +38,7 @@ server <- function(input, output) {
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        bins <- seq(min(x), max(x), length.out = input$bins + 5)
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
